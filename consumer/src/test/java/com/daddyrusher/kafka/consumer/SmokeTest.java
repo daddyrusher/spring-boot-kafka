85c477acbfccf25888eecb75a8f4df2dad228d8e
daddyrusher <daddyrusher@gmail.com>
2024-01-20 16:11:59 +0300

    disable kafka in smoke test

--- consumer/src/test/java/com/daddyrusher/kafka/consumer/SmokeTest.java
+++ consumer/src/test/java/com/daddyrusher/kafka/consumer/SmokeTest.java
@@ -3,7 +3,7 @@ package com.daddyrusher.kafka.consumer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest("spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration")
 public class SmokeTest {
 
     @Test
--- producer/src/test/java/com/daddyrusher/kafka/producer/SmokeTest.java
+++ producer/src/test/java/com/daddyrusher/kafka/producer/SmokeTest.java
@@ -3,7 +3,7 @@ package com.daddyrusher.kafka.producer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest("spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration")
 public class SmokeTest {
 
     @Test
